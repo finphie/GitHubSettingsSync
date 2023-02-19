a11c369d805b8ca46a47e33370a3f060bbecd961
finphie <19237170+finphie@users.noreply.github.com>
2023-02-19 11:38:50 +0900

    プロパティ名修正

--- Source/GitHubSettingsSync.Services.Abstractions/Settings/MergeCommit.cs
+++ Source/GitHubSettingsSync.Services.Abstractions/Settings/MergeCommit.cs
@@ -5,8 +5,8 @@ namespace GitHubSettingsSync.Services.Settings;
 /// <summary>
 /// マージにおけるコミットタイトル・メッセージの種類。
 /// </summary>
-/// <param name="MergeCommitTitle"><inheritdoc cref="GitHubRepository.MergeCommitTitle" path="/summary"/></param>
-/// <param name="MergeCommitMessage"><inheritdoc cref="GitHubRepository.MergeCommitMessage" path="/summary"/></param>
+/// <param name="Title"><inheritdoc cref="GitHubRepository.MergeCommitTitle" path="/summary"/></param>
+/// <param name="Message"><inheritdoc cref="GitHubRepository.MergeCommitMessage" path="/summary"/></param>
 public sealed record MergeCommit(
-    MergeCommitTitleType MergeCommitTitle = MergeCommitTitleType.Unchanged,
-    MergeCommitMessageType MergeCommitMessage = MergeCommitMessageType.Unchanged);
+    MergeCommitTitleType Title = MergeCommitTitleType.Unchanged,
+    MergeCommitMessageType Message = MergeCommitMessageType.Unchanged);
--- Source/GitHubSettingsSync.Services.Abstractions/Settings/SquashMergeCommit.cs
+++ Source/GitHubSettingsSync.Services.Abstractions/Settings/SquashMergeCommit.cs
@@ -5,8 +5,8 @@ namespace GitHubSettingsSync.Services.Settings;
 /// <summary>
 /// スカッシュマージにおけるコミットタイトル・メッセージの種類。
 /// </summary>
-/// <param name="SquashMergeCommitTitle"><inheritdoc cref="GitHubRepository.SquashMergeCommitTitle" path="/summary"/></param>
-/// <param name="SquashMergeCommitMessage"><inheritdoc cref="GitHubRepository.SquashMergeCommitMessage" path="/summary"/></param>
+/// <param name="Title"><inheritdoc cref="GitHubRepository.SquashMergeCommitTitle" path="/summary"/></param>
+/// <param name="Message"><inheritdoc cref="GitHubRepository.SquashMergeCommitMessage" path="/summary"/></param>
 public sealed record SquashMergeCommit(
-    SquashMergeCommitTitleType SquashMergeCommitTitle = SquashMergeCommitTitleType.Unchanged,
-    SquashMergeCommitMessageType SquashMergeCommitMessage = SquashMergeCommitMessageType.Unchanged);
+    SquashMergeCommitTitleType Title = SquashMergeCommitTitleType.Unchanged,
+    SquashMergeCommitMessageType Message = SquashMergeCommitMessageType.Unchanged);
