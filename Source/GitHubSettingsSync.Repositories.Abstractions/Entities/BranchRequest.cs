2872610bf3db5d6b74258cac71c8578842fbf0e3
finphie <19237170+finphie@users.noreply.github.com>
2023-02-20 11:36:21 +0900

    BranchRequest

---
+++ Source/GitHubSettingsSync.Repositories.Abstractions/Entities/BranchRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubSettingsSync.Repositories.Entities;
+
+/// <inheritdoc cref="Request{T}" />
+/// <summary>
+/// ブランチ情報を表すクラスです。
+/// </summary>
+/// <param name="Branch">ブランチ名。</param>
+public sealed record BranchRequest<T>(string Owner, string Name, string Branch, T Settings) : Request<T>(Owner, Name, Settings)
+    where T : notnull;
--- Source/GitHubSettingsSync.Repositories.Abstractions/Entities/Request.cs
+++ Source/GitHubSettingsSync.Repositories.Abstractions/Entities/Request.cs
@@ -6,5 +6,6 @@
 /// <typeparam name="T">設定の型。</typeparam>
 /// <param name="Owner">オーナー名。</param>
 /// <param name="Name">リポジトリ名。</param>
+/// <param name="Settings">設定。</param>
 public record Request<T>(string Owner, string Name, T Settings) : IAggregateRoot
     where T : notnull;
