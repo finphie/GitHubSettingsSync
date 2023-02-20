650fac95996aac0bfd91221f54b0ed21ba0f9ce7
finphie <19237170+finphie@users.noreply.github.com>
2023-02-20 16:21:14 +0900

    コメント追加など

--- Source/GitHubSettingsSync.Repositories.Abstractions/Entities/BranchRequest.cs
+++ Source/GitHubSettingsSync.Repositories.Abstractions/Entities/BranchRequest.cs
@@ -1,9 +1,12 @@
 ﻿namespace GitHubSettingsSync.Repositories.Entities;
 
-/// <inheritdoc cref="Request{T}" />
 /// <summary>
 /// ブランチ情報を表すクラスです。
 /// </summary>
+/// <typeparam name="T">設定の型。</typeparam>
+/// <param name="Owner">オーナー名。</param>
+/// <param name="Name">リポジトリ名。</param>
 /// <param name="Branch">ブランチ名。</param>
+/// <param name="Settings">設定。</param>
 public sealed record BranchRequest<T>(string Owner, string Name, string Branch, T Settings) : Request<T>(Owner, Name, Settings)
     where T : notnull;
--- Source/GitHubSettingsSync.Repositories.Abstractions/Entities/Request.cs
+++ Source/GitHubSettingsSync.Repositories.Abstractions/Entities/Request.cs
@@ -7,5 +7,12 @@
 /// <param name="Owner">オーナー名。</param>
 /// <param name="Name">リポジトリ名。</param>
 /// <param name="Settings">設定。</param>
-public record Request<T>(string Owner, string Name, T Settings) : IAggregateRoot
+public record Request<T>(string Owner, string Name, T Settings) : Request(Owner, Name)
     where T : notnull;
+
+/// <summary>
+/// リポジトリ情報を表すクラスです。
+/// </summary>
+/// <param name="Owner">オーナー名。</param>
+/// <param name="Name">リポジトリ名。</param>
+public record Request(string Owner, string Name) : IAggregateRoot;
