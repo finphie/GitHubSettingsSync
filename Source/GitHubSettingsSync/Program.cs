7f68ec5ce1b32bfbad9a1595745d54d21485e8bb
finphie <19237170+finphie@users.noreply.github.com>
2023-08-11 19:50:50 +0900

    ConfigurationBindingGeneratorを無効化
    クラスにバインドされない不具合があるため

--- Source/GitHubSettingsSync/Program.cs
+++ Source/GitHubSettingsSync/Program.cs
@@ -23,7 +23,11 @@ var builder = ConsoleApp.CreateBuilder(args)
     })
     .ConfigureServices(static (context, services) =>
     {
+#pragma warning disable IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
+#pragma warning disable IL3050 // Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.
         services.Configure<EnvironmentVariables>(context.Configuration);
+#pragma warning restore IL3050 // Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.
+#pragma warning restore IL2026 // Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code
         services.AddSingleton<IValidateOptions<EnvironmentVariables>, EnvironmentVariablesValidator>();
 
         services.AddHttpClient<IGitHubClient, GitHubClient>(static (provider, client) =>
