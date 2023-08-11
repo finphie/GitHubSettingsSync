50b2d44cb01125f7684aa9501832c202bdf07686
finphie <19237170+finphie@users.noreply.github.com>
2023-08-11 14:24:39 +0900

    SA1402を抑制

--- Source/GitHubSettingsSync.Repositories.Abstractions/Entities/Request.cs
+++ Source/GitHubSettingsSync.Repositories.Abstractions/Entities/Request.cs
@@ -1,4 +1,6 @@
-﻿namespace GitHubSettingsSync.Repositories.Entities;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace GitHubSettingsSync.Repositories.Entities;
 
 /// <summary>
 /// リクエスト情報を表すクラスです。
@@ -14,5 +16,6 @@ public record Request(string Owner, string Name) : IRequest;
 /// <param name="Owner">オーナー名。</param>
 /// <param name="Name">リポジトリ名。</param>
 /// <param name="Settings">設定。</param>
+[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "可読性のため。")]
 public sealed record Request<T>(string Owner, string Name, T Settings) : Request(Owner, Name), IRequest<T>
     where T : notnull;
