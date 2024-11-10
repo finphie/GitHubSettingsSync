5b9fdc7f164d4961f60e0bcdc8cf68740dd659f7
finphie <19237170+finphie@users.noreply.github.com>
2024-11-10 14:03:44 +0900

    Update Commands.cs

--- Source/GitHubSettingsSync/Commands.cs
+++ Source/GitHubSettingsSync/Commands.cs
@@ -10,7 +10,26 @@ namespace GitHubSettingsSync;
 /// </summary>
 static class Commands
 {
-    public static async Task RepositoryAsync(
+    /// <summary>
+    /// リポジトリの設定を更新します。
+    /// </summary>
+    /// <param name="repository">-r, "owner/repo" format repository name.</param>
+    /// <param name="hasIssues">Whether to enable issues.</param>
+    /// <param name="hasProjects">Whether to enable Projects.</param>
+    /// <param name="hasWiki">Whether to enable Wiki.</param>
+    /// <param name="hasDiscussions">Whether to enable Discussions.</param>
+    /// <param name="allowMergeCommit">Whether to allow merge commits.</param>
+    /// <param name="allowSquashMerge">Whether to allow squash merges.</param>
+    /// <param name="allowRebaseMerge">Whether to allow rebase merges.</param>
+    /// <param name="allowAutoMerge">Whether to allow auto merges.</param>
+    /// <param name="deleteBranchOnMerge">Whether to delete the branch after merging.</param>
+    /// <param name="allowUpdateBranch">Whether to allow branch updates.</param>
+    /// <param name="mergeCommitTitle">The title of the merge commit.</param>
+    /// <param name="mergeCommitMessage">The message of the merge commit.</param>
+    /// <param name="squashMergeCommitTitle">The title of the squash merge commit.</param>
+    /// <param name="squashMergeCommitMessage">The message of the squash merge commit.</param>
+    /// <returns>このメソッドが完了すると、オブジェクトまたは値は返されません。</returns>
+    public static Task RepositoryAsync(
         string repository,
         bool? hasIssues = null,
         bool? hasProjects = null,
@@ -51,11 +70,25 @@ static class Commands
             SquashMergeCommitMessage = squashMergeCommitMessage
         };
 
-        await client.Repositories.UpdateAsync(repositoryOwner, repositoryName, settings)
-            .ConfigureAwait(false);
+        return client.Repositories.UpdateAsync(repositoryOwner, repositoryName, settings);
     }
 
-    public static async Task BranchProtectionAsync(
+    /// <summary>
+    /// ブランチ保護の設定を更新します。
+    /// </summary>
+    /// <param name="repository">-r, "owner/repo" format repository name.</param>
+    /// <param name="branch">Branch name.</param>
+    /// <param name="enforceAdmins">Whether to enforce for administrators.</param>
+    /// <param name="requiredLinearHistory">Whether to require linear history.</param>
+    /// <param name="allowForcePushes">Whether to allow force pushes.</param>
+    /// <param name="allowDeletions">Whether to allow users with push access to delete the protected branch.</param>
+    /// <param name="requiredConversationResolution">Whether to require conversation resolution before merging.</param>
+    /// <param name="dismissStaleReviews">Whether to dismiss stale reviews when new commits are pushed.</param>
+    /// <param name="requireCodeOwnerReviews">Whether to require code owner reviews.</param>
+    /// <param name="requiredApprovingReviewCount">The number of reviewers required to approve a pull request.</param>
+    /// <param name="requireLastPushApproval">Whether to require approval from someone other than the person who pushed the latest commit.</param>
+    /// <returns>このメソッドが完了すると、オブジェクトまたは値は返されません。</returns>
+    public static Task BranchProtectionAsync(
         string repository,
         string branch,
         bool? enforceAdmins = null,
@@ -95,7 +128,6 @@ static class Commands
             RequiredPullRequestReviews = requiredPullRequestReviews
         };
 
-        await client.Branches.BranchProtection.UpdateAsync(repositoryOwner, repositoryName, branch, settings)
-            .ConfigureAwait(false);
+        return client.Branches.BranchProtection.UpdateAsync(repositoryOwner, repositoryName, branch, settings);
     }
 }
