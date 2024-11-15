2705bed50a211dec15b71d3b038f76fb00f87025
finphie <19237170+finphie@users.noreply.github.com>
2024-11-15 10:43:31 +0900

    Create ConsoleApp.cs

---
+++ Source/GitHubSettingsSync/ConsoleApp.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleAppFramework;
+
+/// <summary>
+/// コンソールアプリケーションを提供します。
+/// </summary>
+[UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "引数に配列を使わないので問題ない")]
+[UnconditionalSuppressMessage("AOT", "IL3050:Calling members annotated with 'RequiresDynamicCodeAttribute' may break functionality when AOT compiling.", Justification = "引数に配列を使わないので問題ない")]
+partial class ConsoleApp;
